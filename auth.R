d51c55d0af7b4d32ccc254fe2c3b9b888268fa8b
Seb Dalgarno <seb@poissonconsulting.ca>
2018-08-02 19:36:24 -0600

    replace auth with slackr

--- auth.R
+++
@@ -1,7 +0,0 @@
-#dropbox
-if(!file.exists('helpers/droptoken.rds')){
-  token <- rdrop2::drop_auth()
-  saveRDS(object = token, file = "helpers/droptoken.rds")
-}
-
-gmailr::use_secret_file("helpers/poisson-shiny-mail-auth.json")
