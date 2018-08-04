c64675d870ef68ec8f580e498ee8e0799129124c
Seb Dalgarno <seb@poissonconsulting.ca>
2018-08-04 16:38:44 -0600

    prepare to deploy

--- functions.R
+++ functions.R
@@ -1,30 +1,3 @@
-create_error <- function(message, header = "random") {
-  showModal(modalDialog(
-    footer = modalButton("Got it"),
-    title = ifelse(header == "random", sample(messages, 1), header),
-    message
-  ))
-}
-
-find_peaks <- function (x, m = 3){
-  shape <- diff(sign(diff(x, na.pad = FALSE)))
-  pks <- sapply(which(shape < 0), FUN = function(i){
-    z <- i - m + 1
-    z <- ifelse(z > 0, z, 1)
-    w <- i + m + 1
-    w <- ifelse(w < length(x), w, length(x))
-    if(all(x[c(z : i, (i + 2) : w)] <= x[i + 1])) return(i + 1) else return(numeric(0))
-  })
-  pks <- unlist(pks)
-  pks
-}
-
-labelMandatory <- function(label) {
-  tagList(
-    label,
-    span("*", class = "mandatory_star")
-  )
-}
 
 inline = function (x) {
   tags$div(style = "display:inline-block;", x)
